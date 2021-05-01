c76781c2d9f3ee19b69c4413614b2077d64b6d51
error23 <error23.d@gmail.com>
2021-05-01 16:35:26 +0200

    feature data sql
    Signed-off-by: error23 <error23.d@gmail.com>

---
+++ SQL/create_feature_data_table.sql
@@ -0,0 +1,24 @@
+-- feature_data
+DROP SEQUENCE IF EXISTS feature_data_seq CASCADE;
+CREATE SEQUENCE feature_data_seq
+	START WITH 1
+	INCREMENT BY 50
+	NO MINVALUE
+	NO MAXVALUE
+	CACHE 1;
+
+DROP TABLE IF EXISTS feature_data CASCADE;
+CREATE TABLE feature_data (
+	id BIGINT DEFAULT nextval('feature_data_seq'),
+	created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+	updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+	owner BIGINT NOT NULL,
+	updated_by BIGINT,
+	version INTEGER NOT NULL DEFAULT 0,
+	feature_name VARCHAR NOT NULL UNIQUE,
+	feature_saved_data JSONB
+
+);
+
+ALTER TABLE feature_data ADD CONSTRAINT pk_feature_data PRIMARY KEY (id);
+
