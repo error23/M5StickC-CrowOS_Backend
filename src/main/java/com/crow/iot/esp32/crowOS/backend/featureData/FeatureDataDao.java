6eccbddae00717b335465a1f55291e216f7a7bc1
error23 <error23.d@gmail.com>
2021-05-01 16:36:11 +0200

    feature data dao
    Signed-off-by: error23 <error23.d@gmail.com>

---
+++ src/main/java/com/crow/iot/esp32/crowOS/backend/featureData/FeatureDataDao.java
@@ -0,0 +1,18 @@
+package com.crow.iot.esp32.crowOS.backend.featureData;
+
+import com.crow.iot.esp32.crowOS.backend.commons.architecture.AbstractDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : error23
+ * Created : 01/05/2021
+ */
+@Repository
+public class FeatureDataDao extends AbstractDao<FeatureData> {
+
+	public FeatureDataDao() {
+
+		super();
+	}
+
+}
