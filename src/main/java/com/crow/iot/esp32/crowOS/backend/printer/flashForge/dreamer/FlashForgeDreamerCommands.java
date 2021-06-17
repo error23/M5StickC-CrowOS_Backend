56a58b610ee57a4e46ff3cfa847f2d74f30356b2
error23 <error23.d@gmail.com>
2021-06-17 22:14:04 +0200

    FlashForgeDreamerCommands
    Signed-off-by: error23 <error23.d@gmail.com>

---
+++ src/main/java/com/crow/iot/esp32/crowOS/backend/printer/flashForge/dreamer/FlashForgeDreamerCommands.java
@@ -0,0 +1,33 @@
+package com.crow.iot.esp32.crowOS.backend.printer.flashForge.dreamer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author : error23
+ * Created : 06/06/2021
+ */
+@Getter
+@AllArgsConstructor
+public enum FlashForgeDreamerCommands {
+
+	HELLO("~M601 S1"),
+	GET_GENERAL_INFO("~M115"),
+	GET_TEMPERATURE("~M105"),
+	GET_POSITIONS("~M114"),
+	SET_COLOR("~M146"),
+	BUY("~M602");
+
+	private String commandValue;
+
+	/**
+	 * Gets {@link FlashForgeDreamerCommands} expected answer value
+	 *
+	 * @return expected answer for this command
+	 */
+	public String getExpectedAnswer() {
+
+		return "CMD " + this.commandValue.substring(1, 5) + " Received.";
+	}
+
+}
