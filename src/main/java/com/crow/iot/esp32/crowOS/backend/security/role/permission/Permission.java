edcc4ca21eb38755ec6bf1583bc797a755a2bbfc
error23 <error23.d@gmail.com>
2021-06-17 22:17:32 +0200

    repair permissions
    Signed-off-by: error23 <error23.d@gmail.com>

--- src/main/java/com/crow/iot/esp32/crowOS/backend/security/role/permission/Permission.java
+++ src/main/java/com/crow/iot/esp32/crowOS/backend/security/role/permission/Permission.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author : error23
@@ -16,5 +17,21 @@ public class Permission {
 	private SecuredResource securedResource;
 
 	private List<Privilege> privileges;
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o) return true;
+		if (! (o instanceof Permission)) return false;
+		Permission that = (Permission) o;
+		return this.getSecuredResource() == that.getSecuredResource() &&
+			this.getPrivileges().equals(that.getPrivileges());
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(this.getSecuredResource(), this.getPrivileges());
+	}
 
 }
